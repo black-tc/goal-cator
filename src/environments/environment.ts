7b8ade345d0939790756d00becd35136dccf017d
Tinashe Chifamba <cwaziebwoi@gmail.com>
2021-02-03 12:49:08 +0200

    file changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,14 +5,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyACdHuPkb6Zn44uUarfTLnLhRm63TTHDMA',
-    authDomain: 'covid-19-tracker-d9719.firebaseapp.com',
-    databaseURL: 'https://covid-19-tracker-d9719.firebaseio.com',
-    projectId: 'covid-19-tracker-d9719',
-    storageBucket: 'covid-19-tracker-d9719.appspot.com',
-    messagingSenderId: '708598758128',
-    appId: '1:708598758128:web:ee42725607f6d0b0126035',
-    measurementId: 'G-LR0C6H6WWY'
+    apiKey: "AIzaSyAY3D9a8MaewowiPZDtZDRQNpfTqkYDnDM",
+    authDomain: "goal-cator-77502.firebaseapp.com",
+    projectId: "goal-cator-77502",
+    storageBucket: "goal-cator-77502.appspot.com",
+    messagingSenderId: "57478693482",
+    appId: "1:57478693482:web:1e56501adb93caf27a8013",
+    measurementId: "G-H9KZ9LJ6LB"
   },
 };
 
